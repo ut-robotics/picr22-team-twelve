4b1f649233e209d1377120861439b7e06446b174
Pille Pärnalaas <pille.tahe1@gmail.com>
2022-09-26 18:48:42 +0300

    new file:   Color.py 	new file:   README.md 	new file:   __pycache__/Color.cpython-39.pyc 	new file:   __pycache__/camera.cpython-310.pyc 	new file:   __pycache__/camera.cpython-39.pyc 	new file:   __pycache__/image_processor.cpython-310.pyc 	new file:   __pycache__/image_processor.cpython-39.pyc 	new file:   __pycache__/motion.cpython-39.pyc 	new file:   camera.py 	new file:   colors/colors.pkl 	new file:   config_colors.py 	new file:   image_processor.py 	new file:   main.py 	new file:   motion.py 	new file:   segment_module/Segment.egg-info/PKG-INFO 	new file:   segment_module/Segment.egg-info/SOURCES.txt 	new file:   segment_module/Segment.egg-info/dependency_links.txt 	new file:   segment_module/Segment.egg-info/top_level.txt 	new file:   segment_module/build/lib.linux-x86_64-cpython-39/segment.cpython-39-x86_64-linux-gnu.so 	new file:   segment_module/build/temp.linux-x86_64-cpython-39/segment.o 	new file:   segment_module/colors/colors.pkl 	new file:   segment_module/segment.c 	new file:   segment_module/setup.py 	new file:   segment_module/test.py

---
+++ software/bootcamp-code/main.py
@@ -0,0 +1,84 @@
+import image_processor
+import camera
+import motion
+import cv2
+import time
+import serial.tools.list_ports
+
+def main_loop():
+    debug = True
+    
+    motion_sim = motion.TurtleRobot()
+    motion_sim2 = motion.TurtleOmniRobot()
+
+    omni_motion = motion.OmniMotionRobot()
+    
+    #camera instance for normal web cameras
+    #cam = camera.OpenCVCamera(id = 2)
+    # camera instance for realsense cameras
+    cam = camera.RealsenseCamera(exposure = 100)
+    
+    processor = image_processor.ImageProcessor(cam, debug=debug)
+
+    processor.start()
+    motion_sim.open()
+    motion_sim2.open()
+
+    # incase permission issues: sudo chmod 666 /dev/tty0
+    # idProduct=5740 to filter the list given by  serial.tools.list_ports.comports(include_links=False)
+    # from there get the correct
+    port = '/dev/ttyACM0'
+    omni_motion.open(port)
+
+    start = time.time()
+    fps = 0
+    frame = 0
+    frame_cnt = 0
+    try:
+        start_time = time.time()
+        while True:
+            # has argument aligned_depth that enables depth frame to color frame alignment. Costs performance
+            processedData = processor.process_frame(aligned_depth=False)
+
+            # This is where you add the driving behaviour of your robot. It should be able to filter out
+            # objects of interest and calculate the required motion for reaching the objects
+
+            frame_cnt +=1
+
+            frame += 1
+            if frame % 30 == 0:
+                frame = 0
+                end = time.time()
+                fps = 30 / (end - start)
+                start = end
+                print("FPS: {}, framecount: {}".format(fps, frame_cnt))
+                print("ball_count: {}".format(len(processedData.balls)))
+                #if (frame_cnt > 1000):
+                #    break
+
+            if debug:
+                debug_frame = processedData.debug_frame
+
+                cv2.imshow('debug', debug_frame)
+
+                k = cv2.waitKey(1) & 0xff
+                if k == ord('q'):
+                    break
+            current_time = time.time()-start_time
+            if current_time < 2:
+                omni_motion.send_commands(20, 0, 0, 0)
+                print("sent1")
+            elif current_time > 2:
+                print("sent2")
+                omni_motion.send_commands(-20, 0, 0, 0)
+
+    except KeyboardInterrupt:
+        print("closing....")
+    finally:
+        cv2.destroyAllWindows()
+        processor.stop()
+        motion_sim.close()
+        motion_sim2.close()
+        omni_motion.close(port)
+
+main_loop()
