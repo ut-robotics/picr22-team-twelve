ff9da5d72f2414781f7a87575fbfaa6bca423d5b
T2heke <pille.tahe1@gmail.com>
2022-10-29 15:37:04 +0300

    Update main.py

--- software/bootcamp-code/main.py
+++ software/bootcamp-code/main.py
@@ -5,12 +5,13 @@ import cv2
 import time
 from enum import Enum
 
-# STATE MACHINE: 3 states - FIND_BALL = 0, CENTER_BALL = 1, DRIVE_TO_BALL = 2
+# STATE MACHINE: 
 class State(Enum):
     FIND_BALL = 0
     CENTER_BALL = 1
-    DRIVE_TO_BALL = 2
-    STOP = 3
+    FIND_BASKET = 2
+    THROW_BALL = 3
+    STOP = 4
 
 def main_loop():
     debug = False
@@ -43,81 +44,110 @@ def main_loop():
     try:
         # time counter for testing
         zero_time = time.time()
-        # Ball desired coordinates are in the middle of the frame.
+        # Ball desired coordinates are in the middle of the frame width and 83% of frame height
+        # (THE ZERO COORDINATES OF THE FRAME ARE IN THE UPPER LEFT CORNER.)
         ball_desired_x = cam.rgb_width/2
         ball_desired_y = cam.rgb_height/1.2
+        # Speed range for motors is 48 - 2047, we use 100 for max motor speed at the moment.
         max_motor_speed = 100
         
         while True:
             # has argument aligned_depth that enables depth frame to color frame alignment. Costs performance
             processedData = processor.process_frame(aligned_depth=False)
-
-            # TODO This is where you add the driving behaviour of your robot. It should be able to filter out
-            # objects of interest and calculate the required motion for reaching the object.
-
+            
+            # Here is the state machine for the driving logic.
+            
             # STATE TO FIND THE BALL
             if state == State.FIND_BALL:
                 print(state)
+                # if we have a ball in view, center it
                 if len(processedData.balls)>0:
                     state=State.CENTER_BALL
                     continue
+                # if no ball found, rotate
                 rotation = max_motor_speed/15
                 omni_motion.move(0, 0, rotation)
 
             # STATE TO CENTER THE BALL
             elif state == State.CENTER_BALL:
                 print(state)
+                # if there are no balls, then find one
                 if len(processedData.balls)<1:
                     state=State.FIND_BALL
                     continue
-                # The biggest ball is the first one in the detected balls list.
-                # Find the coordinates of the largest ball.
-                # (THE ZERO COORDINATES OF THE FRAME ARE IN THE UPPER LEFT CORNER.)
+                    
+                # The biggest ball is the last one in the detected balls list.
+                # Get the coordinates of the largest ball.
                 # Using these coordinates calculate the side speed, forward speed and rotation for the robot.
                 
                 # The destination coordinates are the difference between the ball location and desired location.
-                    #print(processedData.balls)
+                # print(processedData.balls)
                 dest_x = ball_desired_x - processedData.balls[-1].x
                 dest_y = ball_desired_y - processedData.balls[-1].y
 
                 # Normalize destination in the [-1;1] range and and then multiply it with motor max speed.
                 # (then the driving is proportional)
-                # Speed range for motors is 48 - 2047, we use 100 for max motor speed at the moment.
 
-                print(processedData.balls[-1].x)
-                #print(dest_x)
-                print(dest_x/cam.rgb_width)
+                # print(processedData.balls[-1].x)
+                # print(dest_x)
+                # print(dest_x/cam.rgb_width)
+                
+                # if the distance of the biggest ball found, try finding one nearer
                 if processedData.balls[-1].distance>500:
-                    #state=State.STOP
                     state=State.FIND_BALL
+                # if ball is close enough, circle it and find a basket
+                elif processedData.balls[-1].distance<400:
+                    state=State.FIND_BASKET
+                    continue
+                # else drive to and center the ball simultaniously
                 else:
+                    # Normalize destination in the [-1;1] range and and then multiply it with motor max speed.
+                    # (then the driving is proportional)
                     x_speed = (dest_x/cam.rgb_width) * max_motor_speed
                     y_speed = (dest_y/cam.rgb_height) * max_motor_speed
-                    #rotation = 0
+                    # rotation = 0
                     # Drive towards a ball:
                         # side speed is x_speed
                         # forward speed is y_speed
                         # rotation if you want to turn
-                    print("X: ", x_speed, "Y: ", y_speed)
+                    # print("X: ", x_speed, "Y: ", y_speed)
                     omni_motion.move(-1*x_speed, -1*y_speed, x_speed)
-
+            
+            # state to find the basket when ball is found - circle around the ball until basket is found, then move on to throwing
+            elif state==State.FIND_BASKET:
+                print(state)
+                # if there are no balls, then find one
+                if len(processedData.balls)<1:
+                    state=State.FIND_BALL
+                    continue
+                
+                # find blue basket
+                if len(processedData.basket_b>0): # or processedData.basket_m>0)
+                    # center the basket with orbiting
+                else:
+                    
+                # otherwise orbit the ball until basket is found
+                
+                
+            """
             elif state==State.DRIVE_TO_BALL:
                 print(state)
                 if len(processedData.balls)<1:
                     state=state.FIND_BALL
                     continue
+                # if the ball is further than 10cm and not quite centered, center it
                 elif -0.05 > ((ball_desired_x - processedData.balls[-1].x) / cam.rgb_width) > 0.05 \
                         and processedData.balls[0].distance<400:
                     state=state.CENTER_BALL
-                    print((ball_desired_x - processedData.balls[-1].x) / cam.rgb_width)
-                elif processedData.balls[0].distance<400:
+                    # print((ball_desired_x - processedData.balls[-1].x) / cam.rgb_width)
+                # if its close enough drive onto it
+                else processedData.balls[0].distance<400:
                     omni_motion.move(0, -3, 0)
-                else:
-                    state=State.STOP
 
             elif state==State.STOP:
                 print(state)
                 omni_motion.move(0, 0, 0)
+            """
                 
 
             # Mainboard and communication testing function.
